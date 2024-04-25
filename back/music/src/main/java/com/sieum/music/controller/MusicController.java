b049ba77e0e64fc353d9cf929d3974e6a17f437d
허승경 <hhe98hsk@gmail.com>
2024-04-25 16:56:18 +0900

    [Feature] API development -Search Music

--- back/music/src/main/java/com/sieum/music/controller/MusicController.java
+++ back/music/src/main/java/com/sieum/music/controller/MusicController.java
@@ -1,18 +1,27 @@
 package com.sieum.music.controller;
 
+import com.sieum.music.dto.response.SearchSongResponse;
 import com.sieum.music.dto.request.NearItemPointRequest;
+import com.sieum.music.dto.response.SearchSongResponse;
 import com.sieum.music.service.MusicService;
+import com.sieum.music.util.YoutubeMusicUtil;
+import com.sieum.music.util.YoutubeMusicUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class MusicController {
 
     private final MusicService musicService;
+    private final YoutubeMusicUtil youtubeMusicUtil;
 
     @Operation(summary = "show detail of a thrown music")
     @GetMapping("/thrown/{throwId}")
@@ -29,6 +38,13 @@ public class MusicController {
         long userId = musicService.getCurrentUserId(authorization);
         musicService.createPickup(userId, throwId);
         return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "search for songs on YouTube")
+    @GetMapping("/search/{keyword}")
+    public ResponseEntity<?> searchSong(@PathVariable("keyword") String keyword) {
+        List<SearchSongResponse> searchSongResponse = youtubeMusicUtil.searchSongInYoutube(keyword);
+        return ResponseEntity.ok().body(searchSongResponse);
     }
 
     @Operation(summary = "Search for list of dropped music within 600 radius")
--- back/music/src/main/java/com/sieum/music/exception/CustomExceptionStatus.java
+++ back/music/src/main/java/com/sieum/music/exception/CustomExceptionStatus.java
@@ -10,7 +10,8 @@ public enum CustomExceptionStatus implements BaseErrorCode {
     INTERNAL_SERVER_ERROR("InternalServer_500_1", "Server error"),
     INVALID_REQUEST("BadRequest_400_1", "Invalid request"),
     REQUEST_ERROR("NotValidInput_400_2", "Invalid input"),
-    NOT_FOUND_THROW_ITEM_ID("THROW_400_1", "No throw item with the requested id");
+    NOT_FOUND_THROW_ITEM_ID("THROW_400_1", "No throw item with the requested id"),
+    NOT_FOUND_KEY_WORD("Search_400_1", "No search key word");
 
     private String code;
     private String reason;
