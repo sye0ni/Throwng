016806ce11da5a655af26304a9c5375eda98e6e2
강민정 <a50070863@gmail.com>
2024-04-28 17:51:13 +0900

    [Hotfix] Resolve CORS error

--- back/music/src/main/java/com/sieum/music/controller/MusicController.java
+++ back/music/src/main/java/com/sieum/music/controller/MusicController.java
@@ -13,7 +13,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class MusicController {
