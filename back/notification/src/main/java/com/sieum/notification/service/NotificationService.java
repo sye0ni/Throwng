6399e57d74de82b3e40c71d9f3449b7226aa68a8
lsy5490 <lsy5490@naver.com>
2024-04-28 17:46:01 +0900

    feat: create basic code of sending notification using fcm (#82)

---
+++ back/notification/src/main/java/com/sieum/notification/service/NotificationService.java
@@ -0,0 +1,8 @@
+package com.sieum.notification.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {}
