90648ed513826000b4022495eb397ea29e8ba5ad
허승경 <hhe98hsk@gmail.com>
2024-04-30 02:47:59 +0900

    [Feature] Development User History

--- back/user/src/main/java/com/sieum/user/controller/AuthController.java
+++ back/user/src/main/java/com/sieum/user/controller/AuthController.java
@@ -4,11 +4,14 @@ import static org.springframework.http.HttpHeaders.SET_COOKIE;
 import static org.springframework.http.HttpStatus.CREATED;
 
 import com.sieum.user.domain.User;
+import com.sieum.user.domain.UserHistory;
 import com.sieum.user.dto.MemberTokens;
 import com.sieum.user.dto.response.AccessTokenResponse;
+import com.sieum.user.repository.UserHistoryRepository;
 import com.sieum.user.service.LoginService;
 import com.sieum.user.service.MyUserDetailsService;
 import com.sieum.user.service.UserService;
+import com.sieum.user.util.ClientIpUtil;
 import com.sieum.user.util.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import javax.servlet.http.HttpServletRequest;
@@ -32,6 +35,8 @@ public class AuthController {
     private final UserService userService;
     private final JwtUtil jwtUtil;
     private final MyUserDetailsService myUserDetailsService;
+    private final UserHistoryRepository userHistoryRepository;
+    private final ClientIpUtil clientIpUtil;
 
     @GetMapping("/login/{provider}")
     public ResponseEntity<AccessTokenResponse> login(
@@ -40,6 +45,10 @@ public class AuthController {
             @RequestParam("code") String code,
             final HttpServletResponse response) {
         final User user = loginService.login(provider, code);
+
+        userHistoryRepository.save(
+                UserHistory.builder().ip(ClientIpUtil.getClientIP(request)).user(user).build());
+
         final MemberTokens memberTokens = jwtUtil.createJwtToken(user);
         final ResponseCookie cookie =
                 ResponseCookie.from("refresh-token", memberTokens.getRefreshToken())
