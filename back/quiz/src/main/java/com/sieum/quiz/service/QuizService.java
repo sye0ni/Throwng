07d859398dbe476c432d3fd6e1977bc61de95cde
임승연 <lsy5490@naver.com>
2024-05-07 01:11:53 +0900

    [Feat] API development - Create quiz history after answer submitted

--- back/quiz/src/main/java/com/sieum/quiz/service/QuizService.java
+++ back/quiz/src/main/java/com/sieum/quiz/service/QuizService.java
@@ -1,12 +1,21 @@
 package com.sieum.quiz.service;
 
+import static com.sieum.quiz.exception.CustomExceptionStatus.INVALID_QUIZ_ID;
+
 import com.sieum.quiz.controller.feign.TokenAuthClient;
+import com.sieum.quiz.domain.Quiz;
+import com.sieum.quiz.domain.QuizHistory;
 import com.sieum.quiz.domain.enums.CouponRoute;
+import com.sieum.quiz.dto.request.QuizHistoryCreationRequest;
 import com.sieum.quiz.dto.response.CouponIssuanceStatusResponse;
+import com.sieum.quiz.exception.BadRequestException;
 import com.sieum.quiz.repository.CouponReposistory;
+import com.sieum.quiz.repository.QuizHistoryRepository;
+import com.sieum.quiz.repository.QuizRepository;
 import java.time.LocalDate;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +26,8 @@ public class QuizService {
 
     private final TokenAuthClient tokenAuthClient;
     private final CouponReposistory couponRepository;
+    private final QuizRepository quizRepository;
+    private final QuizHistoryRepository quizHistoryRepository;
 
     public List<CouponIssuanceStatusResponse> getCouponIssuanceStatus(final long userId) {
 
@@ -34,6 +45,24 @@ public class QuizService {
                                     .build();
                         })
                 .collect(Collectors.toList());
+    }
+
+    public void createQuizHistory(
+            final long userId, final QuizHistoryCreationRequest quizHistoryCreationRequest) {
+
+        final Optional<Quiz> quiz = quizRepository.findById(quizHistoryCreationRequest.getQuizId());
+
+        if (quiz.isEmpty()) {
+            throw new BadRequestException(INVALID_QUIZ_ID);
+        }
+
+        quizHistoryRepository.save(
+                QuizHistory.builder()
+                        .quiz(quiz.get())
+                        .submit(quizHistoryCreationRequest.getSubmit())
+                        .result(quizHistoryCreationRequest.isResult())
+                        .userId(userId)
+                        .build());
     }
 
     public long getCurrentUserId(final String authorization) {
