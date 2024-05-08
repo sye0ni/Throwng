2060d05066ace75326c5959baab2852bbd60e283
허승경 <hhe98hsk@gmail.com>
2024-05-08 16:55:19 +0900

    [Feature] Develop a feign client verification that validates coupons

--- back/quiz/src/main/java/com/sieum/quiz/repository/CouponReposistory.java
+++ back/quiz/src/main/java/com/sieum/quiz/repository/CouponReposistory.java
@@ -3,6 +3,7 @@ package com.sieum.quiz.repository;
 import com.sieum.quiz.domain.Coupon;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponReposistory extends JpaRepository<Coupon, Long> {
@@ -11,4 +12,6 @@ public interface CouponReposistory extends JpaRepository<Coupon, Long> {
             final LocalDateTime localDateTime, final String byName, final long userId);
 
     List<Coupon> findByUserId(final long userId);
+
+    Optional<Coupon> findByIdAndUserId(final long id, final long userId);
 }
