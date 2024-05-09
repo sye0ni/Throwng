d4df08ad2ebe0eec0519b82b3d31e509ed6c035d
허승경 <hhe98hsk@gmail.com>
2024-05-10 01:04:47 +0900

    [Feature] Develop user-level rating upgrades based on experience points in user-component

--- back/user/src/main/java/com/sieum/user/repository/LevelRepository.java
+++ back/user/src/main/java/com/sieum/user/repository/LevelRepository.java
@@ -3,4 +3,6 @@ package com.sieum.user.repository;
 import com.sieum.user.domain.Level;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LevelRepository extends JpaRepository<Level, Integer> {}
+public interface LevelRepository extends JpaRepository<Level, Integer> {
+    Level findByGrade(final int grade);
+}
