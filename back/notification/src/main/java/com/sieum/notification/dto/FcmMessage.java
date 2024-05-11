bbd4d4dbbde24f58c06bd11100d112004c7af478
lsy5490 <lsy5490@naver.com>
2024-05-11 16:15:17 +0900

    test: dto class for test(#82)

--- back/notification/src/main/java/com/sieum/notification/dto/FcmMessage.java
+++ back/notification/src/main/java/com/sieum/notification/dto/FcmMessage.java
@@ -8,24 +8,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class FcmMessage {
-
-    private boolean validateOnly;
-    private Message message;
-
-    @Builder
-    @AllArgsConstructor
-    @Getter
-    public static class Message {
-        private Notification notification;
-        private String token;
-    }
-
-    @Builder
-    @AllArgsConstructor
-    @Getter
-    public static class Notification {
-        private String title;
-        private String body;
-        private String image;
-    }
+    private String link;
+    private String title;
+    private String body;
+    private String image;
 }
