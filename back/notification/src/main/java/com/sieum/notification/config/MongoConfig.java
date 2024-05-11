de0639fc75a2e237445c19bdcd45da2649ce94b2
lsy5490 <lsy5490@naver.com>
2024-05-11 16:16:07 +0900

    refactor: apply spotless(#82)

--- back/notification/src/main/java/com/sieum/notification/config/MongoConfig.java
+++ back/notification/src/main/java/com/sieum/notification/config/MongoConfig.java
@@ -4,6 +4,8 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.connection.ConnectionPoolSettings;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
