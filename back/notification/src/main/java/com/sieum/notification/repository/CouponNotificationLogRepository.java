6822126d28b9dcdccc45afa5789a2a40f035ac1a
lsy5490 <lsy5490@naver.com>
2024-05-12 13:02:17 +0900

    feat: add feign client to notification component to get user list (#349)

--- back/notification/src/main/java/com/sieum/notification/repository/CouponNotificationLogRepository.java
+++ back/notification/src/main/java/com/sieum/notification/repository/CouponNotificationLogRepository.java
@@ -1,7 +1,11 @@
 package com.sieum.notification.repository;
 
 import com.sieum.notification.domain.CouponNotificationLog;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CouponNotificationLogRepository
-        extends MongoRepository<CouponNotificationLog, String> {}
+        extends MongoRepository<CouponNotificationLog, String> {
+
+    List<CouponNotificationLog> findAllByUserId(final long userId);
+}
