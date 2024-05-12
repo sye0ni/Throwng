8dbf88e4950faa3968262886c5a2820d0e535cbb
강민정 <a50070863@gmail.com>
2024-05-12 16:44:43 +0900

    [Feature] Development of batch to determine popular music

--- back/batch/src/main/java/com/sieum/batch/feign/MusicFeignClient.java
+++ back/batch/src/main/java/com/sieum/batch/feign/MusicFeignClient.java
@@ -2,9 +2,13 @@ package com.sieum.batch.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @FeignClient(name = "MUSIC")
 public interface MusicFeignClient {
     @DeleteMapping("/music/throw-items")
     long deleteNotFamousMusic();
+
+    @PutMapping("/music/batch/popular")
+    void popularMusic();
 }
