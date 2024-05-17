b9a56f86c8cb77605901a0f1a27302f9382c41df
김민준 <yid0816@naver.com>
2024-05-17 17:31:41 +0900

    feat: Setting FE Sentry (#435)

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,5 +1,29 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
+import React from "react";
+import * as Sentry from "@sentry/react";
+import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from "react-router-dom";
+
+Sentry.init({
+  dsn: "https://e59c6078b8161c146a11dc1478f67e84@o4507270245777408.ingest.de.sentry.io/4507270261112912",
+  integrations: [
+    Sentry.reactRouterV6BrowserTracingIntegration({
+        useEffect: React.useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes
+    }),
+    Sentry.replayIntegration()
+  ],
+  tracesSampleRate: 0.25,
+  tracePropagationTargets: [
+    "localhost:5173",
+    /^https:\/\/www\.sieum\.co\.kr/,
+  ],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -5,65 +6,69 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
-      manifest: {
-        name: "Throwng",
-        short_name: "Throwng",
-        display: "standalone",
-        theme_color: "#0F1114",
-        background_color: "#0F1114",
-        description: "Song Drop&Pick",
-        orientation: "portrait-primary",
-        start_url: "/",
-        prefer_related_applications: true,
-        icons: [
-          {
-            src: "./icons/logo192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "./icons/logo192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "./icons/logo192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: "./icons/logo512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "./icons/logo512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "./icons/logo512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react(), tsconfigPaths(), VitePWA({
+    registerType: "autoUpdate",
+    devOptions: {
+      enabled: true,
+    },
+    manifest: {
+      name: "Throwng",
+      short_name: "Throwng",
+      display: "standalone",
+      theme_color: "#0F1114",
+      background_color: "#0F1114",
+      description: "Song Drop&Pick",
+      orientation: "portrait-primary",
+      start_url: "/",
+      prefer_related_applications: true,
+      icons: [
+        {
+          src: "./icons/logo192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "./icons/logo192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "any",
+        },
+        {
+          src: "./icons/logo192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "./icons/logo512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "./icons/logo512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any",
+        },
+        {
+          src: "./icons/logo512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+      ],
+    },
+  }), sentryVitePlugin({
+    org: "throwng",
+    project: "javascript-react"
+  })],
+
   server: {
     port: 5173,
     host: "0.0.0.0",
   },
+
+  build: {
+    sourcemap: true
+  }
 });
