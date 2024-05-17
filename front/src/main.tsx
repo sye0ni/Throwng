17ce1181d85709803748597d58a772b1dd652e9a
정태윤 <wjdxodbs52@naver.com>
2024-05-17 15:12:42 +0900

    feat: developing (#434)

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,4 +2,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const url = window.location.pathname;
+
+if (url !== "/grafana-server") {
+  ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+} else {
+  window.location.href = url;
+}
