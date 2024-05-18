533b6b3a24e3416b1d64119c8539f44327d0c588
김민준 <yid0816@naver.com>
2024-05-18 19:22:31 +0900

    [Fix] Nickname Change Coupon Error Fix

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,7 +6,13 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), VitePWA({
+  plugins: [
+    sentryVitePlugin({
+      org: "throwng",
+      project: "javascript-react",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+    react(), tsconfigPaths(), VitePWA({
     registerType: "autoUpdate",
     devOptions: {
       enabled: true,
@@ -58,14 +64,8 @@ export default defineConfig({
         },
       ],
     },
-  }), sentryVitePlugin({
-    org: "throwng",
-    project: "javascript-react",
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-  }), sentryVitePlugin({
-    org: "throwng",
-    project: "javascript-react"
-  })],
+  }),
+],
 
   server: {
     port: 5173,
